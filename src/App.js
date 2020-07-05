614dec3d784a4b8f4bd5ee419ad715967c6336d9
Ubuntu <jhhernan@TopV1.khir1z4s25dupjpsqc1zuq4g5a.bx.internal.cloudapp.net>
2020-07-05 14:14:41 +0000

    Se despliegan  nuevas tareas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      list : [],
+      newTask : "",
+    }
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      newTask: event.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let newList = this.state.list.concat(this.state.newTask);
+    this.setState({
+      list: newList,
+      newTask: "",
+    })
+
+  }
+
+
   render() {
     return (
       <div className="wrapper">
@@ -12,9 +37,12 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.list.map((item,idx) => 
+              <li key={idx}>{item}</li>
+              )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.handleSubmit}>
+             <input type="text" id="new-task" value={this.state.newTask} onChange={this.handleInput} placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
